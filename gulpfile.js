4b935add1559753916f532de7b433b5b52f5243e
Konstantin Kovalev <constantiner@gmail.com>
2018-06-07 14:15:49 +0200

    Minification support

--- gulpfile.js
+++ gulpfile.js
@@ -16,6 +16,7 @@ const babel = require("gulp-babel");
 const del = require("del");
 const sourcemaps = require("gulp-sourcemaps");
 const concat = require("gulp-concat");
+const uglify = require("gulp-uglify");
 const file = require("gulp-file");
 const BABEL_PLUGINS = ["transform-es2015-modules-umd"];
 const BROWSERS = [">0.25%", "not ie 11", "not op_mini all"];
@@ -60,12 +61,36 @@ gulp.task("proceedEs5umd", ["emptyBannerFile"], () =>
 
 gulp.task("es5umd", ["proceedEs5umd"], () => del(["tmp"]));
 
+gulp.task("es5umdMinified", () =>
+	getSourceFile()
+		.pipe(sourcemaps.init())
+		.pipe(concat(`${MAIN_FILE_NAME}.min.js`))
+		.pipe(
+			babel({
+				presets: [
+					[
+						"env",
+						{
+							targets: {
+								browsers: BROWSERS
+							}
+						}
+					]
+				],
+				plugins: BABEL_PLUGINS
+			})
+		)
+		.pipe(uglify())
+		.pipe(sourcemaps.write("."))
+		.pipe(getDest())
+);
+
 gulp.task("emptyBannerFile", () =>
 	file("./tmp/banner.js", "", { src: true })
 		.pipe(getBanner())
 		.pipe(gulp.dest("."))
 );
 
-gulp.task("scripts", () => gulp.start("es6module", "es5umd"));
+gulp.task("scripts", () => gulp.start("es6module", "es5umd", "es5umdMinified"));
 
 gulp.task("default", ["clean"], () => gulp.start("scripts"));
