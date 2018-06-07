93d260e3df0fa9050d6d3d1a5f3dd0c3159a440b
Konstantin Kovalev <constantiner@gmail.com>
2018-06-07 14:08:41 +0200

    Sourcemaps fixed on build

--- gulpfile.js
+++ gulpfile.js
@@ -15,10 +15,15 @@ const banner = `/**
 const babel = require("gulp-babel");
 const del = require("del");
 const sourcemaps = require("gulp-sourcemaps");
+const concat = require("gulp-concat");
+const file = require("gulp-file");
+const BABEL_PLUGINS = ["transform-es2015-modules-umd"];
+const BROWSERS = [">0.25%", "not ie 11", "not op_mini all"];
+const MAIN_FILE_NAME = "cram-md5-digest";
 
 gulp.task("clean", () => del(["dist"]));
 
-const getSourceFile = () => gulp.src("src/cram-md5-digest.js"),
+const getSourceFile = () => gulp.src(`src/${MAIN_FILE_NAME}.js`),
 	getBanner = () => header(banner, { pkg }),
 	getDest = () => gulp.dest("./dist/");
 
@@ -29,8 +34,9 @@ gulp.task("es6module", () =>
 		.pipe(getDest())
 );
 
-gulp.task("es5umd", () =>
-	getSourceFile()
+gulp.task("proceedEs5umd", ["emptyBannerFile"], () =>
+	gulp
+		.src(["tmp/banner.js", "src/**/*.js"])
 		.pipe(sourcemaps.init())
 		.pipe(
 			babel({
@@ -39,17 +45,25 @@ gulp.task("es5umd", () =>
 						"env",
 						{
 							targets: {
-								browsers: [">0.25%", "not ie 11", "not op_mini all"]
+								browsers: BROWSERS
 							}
 						}
 					]
 				],
-				plugins: ["transform-es2015-modules-umd"]
+				plugins: BABEL_PLUGINS
 			})
 		)
-		.pipe(getBanner())
+		.pipe(concat(`${MAIN_FILE_NAME}.js`))
 		.pipe(sourcemaps.write("."))
 		.pipe(getDest())
+);
+
+gulp.task("es5umd", ["proceedEs5umd"], () => del(["tmp"]));
+
+gulp.task("emptyBannerFile", () =>
+	file("./tmp/banner.js", "", { src: true })
+		.pipe(getBanner())
+		.pipe(gulp.dest("."))
 );
 
 gulp.task("scripts", () => gulp.start("es6module", "es5umd"));
