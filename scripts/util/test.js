b38ef9bc857ec87ad2208d5185c495f3f90c6ece
Konstantin Kovalev <constantiner@gmail.com>
2020-04-28 18:54:53 +0200

    Add watch mode support for tests

---
+++ scripts/util/test.js
@@ -0,0 +1,58 @@
+import { getConfigFileSync } from "@constantiner/resolve-node-configs-hierarchy";
+import childProcess from "child_process";
+import { run } from "jest";
+
+const getJestConfigFile = jestConfigFile => {
+	const configPath = jestConfigFile;
+	return getConfigFileSync(configPath, true);
+};
+function isInGitRepository(execSync) {
+	try {
+		execSync("git rev-parse --is-inside-work-tree", { stdio: "ignore" });
+		return true;
+	} catch (e) {
+		return false;
+	}
+}
+
+function isInMercurialRepository(execSync) {
+	try {
+		execSync("hg --cwd . root", { stdio: "ignore" });
+		return true;
+	} catch (e) {
+		return false;
+	}
+}
+
+const getArgv = (jestConfigFile, runInBand, watch) => {
+	const execSync = childProcess.execSync;
+	let argv = process.argv.slice(2);
+
+	// Watch unless on CI, in coverage mode, or explicitly running all tests
+	if (watch && !process.env.CI && !argv.includes("--coverage") && !argv.includes("--watchAll")) {
+		// https://github.com/facebook/create-react-app/issues/5210
+		const hasSourceControl = isInGitRepository(execSync) || isInMercurialRepository(execSync);
+		argv.push(hasSourceControl ? "--watch" : "--watchAll");
+	}
+
+	if (!argv.includes("--config")) {
+		const configPath = getJestConfigFile(jestConfigFile);
+		if (configPath) {
+			argv.push("--config");
+			argv.push(configPath);
+		}
+	}
+
+	if (runInBand) {
+		argv.push("--runInBand");
+	}
+
+	return argv;
+};
+
+const runJest = (jestConfigFile, runInBand = false, watch = true) => {
+	const argv = getArgv(jestConfigFile, runInBand, watch);
+	run(argv);
+};
+
+export default runJest;
